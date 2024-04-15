0048001697dba735732c30f27504a78a6a826906
kavikumar k <95954876+kavikumarkkk@users.noreply.github.com>
2024-04-15 10:29:12 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# ONLINE-SHOPPING-E-COMMERCE-PLATFORM-PROJECT-REPORT
-ONLINE SHOPPING E-COMMERCE PLATFORM PROJECT REPORT
+#  Shop It Up
+# multi-vendor-ecommerce-website
+A multi-vendor ecommerce store is a marketplace where multiple sellers come together to sell their products and services to multiple customers. In multiple-vendor marketplaces, customers have a chance to buy products from different sellers or brands
---
+++ admin/pnl_catalog.php
@@ -0,0 +1,103 @@
+<?php
+session_start();
+include ('../db.php');
+
+if(!isset($_SESSION["sess_id"])) {
+	header ('location: ../index');
+} else {
+	if ($_SESSION["sess_status"] != 'admin') {
+		header ('location: ../index');
+	} 
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" href="../groceries.ico">
+	<title>admin@siu</title>
+	<link rel="stylesheet" href="../bootstrap/css/all.min.css">
+	<link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+	<script src="../bootstrap/js/jquery-3.4.1.min.js"></script>
+</head>
+<body style="background-color:powderblue;">
+
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <a class="navbar-brand" href="#"><img src="groceries.ico" alt="Shop It Up" width="30" height= auto> Shop It Up</a>
+            <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item ">
+                    <a class="nav-link" href="pnl_user">User Panel</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="pnl_user">Order Panel </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link active" href="pnl_catalog">Catalog Panel</a>
+                </li>
+				</ul>
+				<div class="my-2 my-lg-0">
+                <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <a class="btn btn-outline-success my-2 my-sm-0"" href="../action?act=lgout">Logout</a>
+                </li>
+				</ul>
+            </div>
+           
+            
+            </div>
+        </div>
+    </nav>
+
+	<div class="container">
+		
+
+
+		<div class="row p-4">
+			<div class="col">
+				<h2>all catalog list</h2>
+				<table class="table">
+					<tr>
+						<th>no.</th>
+						<th>picture</th>
+						<th>name</th>
+						<th>price</th>
+						<th>description</th>
+						<th>shop name</th>
+					</tr>
+
+					<?php
+					$query = "SELECT * from fds_ctlog";
+					$result = mysqli_query($conn, $query);
+					$count = 1;
+
+					if (mysqli_num_rows($result) > 0) {
+						while($row = mysqli_fetch_assoc($result)){
+
+							echo '<tr>';
+							echo '<td>' . $count++ . '</td>';
+							echo '<td><img class="img-fluid" width="150" src="../img/menu/'. $row['ctlog_img'] .'" alt="no image"></td>';
+							echo '<td>' . $row['ctlog_nme'] . '</td>';
+							echo '<td>' . $row['ctlog_prc'] . '</td>';
+							echo '<td>' . $row['ctlog_desc'] . '</td>';
+							echo '<td>' . $row['ctlog_shp'] . '</td>';
+						}
+					}
+
+					?>
+				</table>
+			</div>
+		</div>
+
+	</div>
+
+</body>
+</html>
